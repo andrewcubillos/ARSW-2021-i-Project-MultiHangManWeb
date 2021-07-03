eae8108705bb783a95c1ad3af256b30f63e73b78
andrewcubillos <felipecubillos010@gmail.com>
2021-07-02 20:44:38 -0500

    conecurrente

--- src/main/java/co/escuelaing/edu/HangManWeb/WBAppController.java
+++ src/main/java/co/escuelaing/edu/HangManWeb/WBAppController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.escuelaing.edu.wbiinteractiverealtimelive;
+package co.escuelaing.edu.HangManWeb;
 
 import java.util.Collections;
 import org.springframework.boot.SpringApplication;
