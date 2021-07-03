7e64247738e052bea2c478e21d7d18689c8c7b92
andrewcubillos <felipecubillos010@gmail.com>
2021-07-03 00:26:23 -0500

    p

--- src/main/resources/static/js/WBComponents.jsx
+++ src/main/resources/static/js/WBComponents.jsx
@@ -43,12 +43,12 @@ class WBCanvas extends React.Component {
                 button1.position(input1.x + input1.width, 105);
                 
                 input2=p.createInput();
-                input2.position(150,105);
+                input2.position(230,105);
                 button2=p.createButton('submit');
                 button2.position(input2.x + input2.width, 105);
                 
                 input3=p.createInput();
-                input3.position(270,105);
+                input3.position(330,105);
                 button3=p.createButton('submit');
                 button3.position(input3.x + input3.width, 105);
                 
