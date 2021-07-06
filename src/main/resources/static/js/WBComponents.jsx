9ce3844cc12a63034c54351e1ca9ff12161ebac8
andrewcubillos <felipecubillos010@gmail.com>
2021-07-05 19:37:01 -0500

    botones

--- src/main/resources/static/js/WBComponents.jsx
+++ src/main/resources/static/js/WBComponents.jsx
@@ -142,10 +142,10 @@ class WBCanvas extends React.Component {
                    
                     p.line(x1, y3, x5, y5);   
                     wsreference.sendeline(x1,y3,x5,y5,c1,c2,c3);
-                    p.line(5, 8, 50, 100);   
+                    p.line(x1-10, yi+10, x1+10, y2-10);   
                     wsreference.sendeline(x1-10, yi+10, x1+10, y2-10,c1,c2,c3);
                     p.line(x1+10, yi+10, x1-10, y2-10);   
-                    wsreference.sendeline(x1+10, yi+10, x1-10, y2-10,c1,c2,c3);
+                    wsreference.sendeline(x1+10, yi, x1-10, y2-10,c1,c2,c3);
                     
                     
                 }
