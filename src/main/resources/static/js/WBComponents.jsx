1109a261b2e21a6c021d40f6e0013e418fb4047e
andrewcubillos <felipecubillos010@gmail.com>
2021-07-06 15:03:26 -0500

    input

--- src/main/resources/static/js/WBComponents.jsx
+++ src/main/resources/static/js/WBComponents.jsx
@@ -26,7 +26,7 @@ class WBCanvas extends React.Component {
                          console.log("On func call back ", msg);
                         this.drawEllipse(obj.xi, obj.yi,obj.color1,obj.color2,obj.color3);
                         this.drawLine(obj.x1, obj.y1,obj.x2, obj.y2,obj.color1,obj.color2,obj.color3);
-                        this.drawWord(obj.mts,obj.xx,obj.yy);
+                        this.drawWord(obj.mst,obj.xx,obj.yy);
                         
                 });
         
@@ -334,8 +334,7 @@ class WSBBChannel {
         this.wsocket.send(msg);
     }
     sendeword(mst,xx,yy) {
-        let mts= "'"+mst+ "'";
-        let msg = '{ "mst": ' + ( mts) + ', "xx": ' + (xx)  +', "yy": ' + (yy)+ "}";
+        let msg = '{ "mst": ' + (String(mst)) + ', "xx": ' + (xx)  +', "yy": ' + (yy)+ "}";
         console.log("sending: ", msg);
         this.wsocket.send(msg);
     }
