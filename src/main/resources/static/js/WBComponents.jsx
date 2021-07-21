3aa1f4507fc8507901281901fbe653dff2c502c6
andrewcubillos <felipecubillos010@gmail.com>
2021-07-20 20:43:07 -0500

    palabras

--- src/main/resources/static/js/WBComponents.jsx
+++ src/main/resources/static/js/WBComponents.jsx
@@ -279,9 +279,9 @@ class WBCanvas extends React.Component {
 const palabras=paisses();
    const mino = 0;
    const maxi = 350;
-   let rand =  mino + (Math.random() * (maxi-mino));
+   let rand =  mino + Math.Floor(Math.random() * (maxi-mino));
    console.log(rand);
-const word=Array.from(palabras[rand]);
+const word=Array.from(palabras[0]);
 const word2=Array.from(palabras[1]);
 const word3=Array.from(palabras[4]);
 const palabra1= new Array(word.length);
