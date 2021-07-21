f797061c47a412c0a2e3b74f131567ed52acd600
andrewcubillos <felipecubillos010@gmail.com>
2021-07-21 15:11:45 -0500

    palabras

--- src/main/resources/static/js/WBComponents.jsx
+++ src/main/resources/static/js/WBComponents.jsx
@@ -173,7 +173,7 @@ class WBCanvas extends React.Component {
                     p.textSize(47);
                     p.noStroke();
                     p.fill(0);
-                    text('you lose', 350, 100);
+                    p.text('you lose', 350, 100);
                     //p.line(x1, yi-10, x1+20, y2-30);   
                     //wsreference.sendeline(x1, yi-10, x1+20, y2-30,c1,c2,c3);
                     //p.line(x1+20, yi-10, x1, y2-30);   
